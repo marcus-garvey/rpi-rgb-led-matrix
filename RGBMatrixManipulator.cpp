a7fac1fa75a42eca285c5faf88ac1e37810fbe65
marcus-garvey <marc13@gmx.de>
2014-06-20 21:43:20 +0000

    refactoring

---
+++ RGBMatrixManipulator.cpp
@@ -0,0 +1,6 @@
+
+
+#include "RGBMatrixManipulator.h"
+
+
+
--- main.cpp
+++ main.cpp
@@ -8,34 +8,11 @@
 #include "thread.h"
 #include "led-matrix.h"
 #include "PPMImage.h"
+#include "RGBMatrixManipulator.h"
 using std::min;
 using std::max;
 
-// Base-class for a Thread that does something with a matrix.
-class RGBMatrixManipulator : public Thread {
-public:
-  RGBMatrixManipulator(RGBMatrix *m) : running_(true), matrix_(m) {}
-  virtual ~RGBMatrixManipulator() { running_ = false; }
 
-  // Run() implementation needs to check running_ regularly.
-
-protected:
-  volatile bool running_;  // TODO: use mutex, but this is good enough for now.
-  RGBMatrix *const matrix_;
-};
-
-// Pump pixels to screen. Needs to be high priority real-time because jitter
-// here will make the PWM uneven.
-class DisplayUpdater : public RGBMatrixManipulator {
-public:
-  DisplayUpdater(RGBMatrix *m) : RGBMatrixManipulator(m) {}
-
-  void Run() {
-    while (running_) {
-      matrix_->UpdateScreen();
-    }
-  }
-};
 
 class SimpleSquare : public RGBMatrixManipulator {
 public:
